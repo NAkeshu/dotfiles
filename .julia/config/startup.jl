2890f721593bbe35955cec686f2153f0482e711c
TyouKajyu <tyoukajyu@gmail.com>
2023-01-20 14:11:20 +0900

    remove JULIA_NUM_THREADS environment variable from startup.jl

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -6,9 +6,6 @@ if "Manifest.toml" in readdir(".") || issubset(["src", "Project.toml"], readdir(
     Pkg.activate(".")
 end
 
-# multi-threads
-ENV["JULIA_NUM_THREADS"] = "auto"
-
 # set path of conda & py
 ENV["CONDA_JL_NAME"] = "cv"
 ENV["CONDA_JL_HOME"] = ENV["HOME"] * "/miniconda3/envs/" * ENV["CONDA_JL_NAME"]
